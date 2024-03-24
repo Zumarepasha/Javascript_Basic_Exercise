5f0bb577e5c951696afd69064bda04eabebc256a
Zumare Pasha <zumarepasha000@gmail.com>
2024-03-24 14:19:12 +0530

    final commit

---
+++ Basics_of_JS_1/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("zumare")
+
+
+let name = "zumare"
+let age = 22
+let isLoggedIn = false
+let state;
+
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
